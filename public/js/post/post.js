9e2c4edf1e0643cc52a12f6650da4bf0716b4f8c
christopher moutendy <christophermoutendy@gmail.com>
2023-08-27 01:47:53 +0200

    modifgier

--- public/js/post/post.js
+++ public/js/post/post.js
@@ -1,3 +1,6 @@
+const dropdown = document.querySelector('.comment-options-dropdown');
+dropdown.style.display = 'none';
+
 window.onload = function() {
     afficherDonnees();
 };
@@ -5,23 +8,115 @@ window.onload = function() {
 function afficherDonnees() {
     var xhr = new XMLHttpRequest();
     var donnees;
+    let descfor = "";
+    let i = 0;
+    let ipost = 0;
+    var cardContainer = document.getElementById("card-container");
+
 
     xhr.onreadystatechange = function() {
 
         if (xhr.readyState == 4 && xhr.status == 201) {
 
             donnees = JSON.parse(xhr.responseText);
-            console.log(donnees);
-            donnees.forEach(function(element) {
-                var desc = document.getElementById('desc');
-                desc.innerHTML = element.desc; // Supposons que "nom" soit un champ de données.
-                console.log(element.desc);
-            });
+
+
+            for (; i < donnees.length; i++) {
+
+                console.log(donnees);
+
+
+                var card = document.createElement("div");
+                card.classList.add("card");
+
+                var header = document.createElement("div");
+                header.classList.add("header");
+
+                var media = document.createElement("div");
+                media.classList.add("media");
+
+                var headerleft = document.createElement("div");
+                headerleft.classList.add("media-left");
+
+                var post_name = document.createElement("div");
+                post_name.classList.add("post-name");
+
+                var title_is_5 = document.createElement("div");
+                title_is_5.classList.add("title");
+                title_is_5.classList.add("is-9");
+                title_is_5.textContent = donnees[i].user.name;
+                var subtitle = document.createElement("div");
+                subtitle.classList.add("subtitle");
+                subtitle.classList.add("is-6");
+
+
+
+                var figure = document.createElement("figure");
+                figure.classList.add("image");
+                figure.classList.add("is-48x48");
+
+                var imghead = document.createElement("img");
+                imghead.classList.add("imageprofil");
+                imghead.src = donnees[i].user.image_profil;
+
+                var icon_menu = document.createElement("i");
+                icon_menu.classList.add("material-icons");
+                icon_menu.classList.add("posi-icons");
+                icon_menu.classList.add("imageprofil");
+                icon_menu.querySelector("more-icon")
+                icon_menu.textContent = "more_horiz";
+
+
+
+                menu(icon_menu, donnees[i].id)
+                header.appendChild(media);
+                media.appendChild(headerleft);
+                media.appendChild(post_name);
+                headerleft.appendChild(figure);
+                headerleft.appendChild(icon_menu);
+                figure.appendChild(imghead);
+                post_name.appendChild(title_is_5);
+                post_name.appendChild(subtitle);
+
+
+
+                var card_image = document.createElement("div");
+                card_image.classList.add("card-image");
+                var title = document.createElement("div");
+                title.classList.add("text-post");
+                title.textContent = donnees[i].desc;
+                var figurepost = document.createElement("figure");
+                figurepost.classList.add("image");
+                figurepost.classList.add("is-4by3");
+                var img = document.createElement("img");
+                img.classList.add("imagepost");
+                img.src = donnees[i].image;
+
+
+                card_image.appendChild(title);
+                figurepost.appendChild(img);
+                card_image.appendChild(figurepost);
+
+                card.appendChild(header);
+                card.appendChild(card_image);
+
+
+                cardContainer.appendChild(card);
+            }
 
 
         }
     };
 
     xhr.open('GET', '/api/post', true);
+
     xhr.send();
+}
+
+function menu(moreIcon, id) {
+    moreIcon.addEventListener("click", function() {
+        console.log(id);
+        dropdown.style.display = dropdown.style.display ? 'block' : 'none';
+    });
+
 }
--- resources/views/layouts/post.blade.php
+++ resources/views/layouts/post.blade.php
@@ -2,69 +2,13 @@
 
 @section('content')
 <div class="columns body-columns">
-    <div class="column is-half is-offset-one-quarter">
-        <div class="card">
-            <div class="header">
-                <div class="media">
-                    <div class="media-left">
-                        <figure class="image is-48x48">
-                            <img class="imageprofil" src="https://res.cloudinary.com/ameo/image/upload/v1639144778/typocat_svbspx.png" alt="small placeholder image">
-                        </figure>
-                    </div>
-                    <div class="post-name">
-                        <p class="title is-5">John Smith</p>
-                        <p class="subtitle is-6">@johnsmith</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="card-image">
-                <div class="text-post"  id="desc">
-                   dddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec iaculis mauris.
-                    <a>@bulmaio</a>.
-                    <a href="#">#css</a>
-                    <a href="#">#responsive</a>
-                    <br>
-                    <time datetime="2018-1-1">11:09 PM - 1 Jan 2023</time>
-                </div>
-
-                <figure class="image is-4by3">
-                    <img class="imagepost" src="https://cdn.pixabay.com/photo/2023/05/14/02/15/squirrel-7991828_1280.jpg" alt="Placeholder image">
-                </figure>
-            </div>
-            <div class="card-content">
-
-                <div class="level is-mobile">
-
-                    <div class="level-left">
-                        <div class="level-item has-text-centered">
-                            <a href="">
-                                <i class="material-icons">favorite_border</i>
-                            </a>
-                        </div>
-                        <div class="content">
-                            <p>
-                                <strong class="likepost">33 </strong>
-                            </p>
-                        </div>
-                        <div class="level-item has-text-centered">
-
-                                <a href="" data-bs-toggle="modal" data-bs-target="#myModal">
-                                    <i class="material-icons">chat_bubble_outline</i>
-                                </a>
-                                <strong class="commentaire">2 </strong>
-                        </div>
-
-
-
-
-                    </div>
-                </div>
-
-
-            </div>
-        </div>
-      </div>
+    <div class="column is-half is-offset-one-quarter" id="card-container">
+          <div class="comment-options-dropdown">
+            <ul>
+              <li>Modifier</li>
+              <li>Supprimer</li>
+            </ul>
+          </div>
     </div>
    </div>
 </div>
